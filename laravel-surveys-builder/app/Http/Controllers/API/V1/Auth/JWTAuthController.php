644dfc72be7fccbd2028129364dfb92d23a117e7
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-06-17 16:46:42 +0300

    feat : jwt authentication

---
+++ laravel-surveys-builder/app/Http/Controllers/API/V1/Auth/JWTAuthController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+use App\Http\Controllers\Controller;
+use JWTAuth;
+use Validator;
+use Auth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Symfony\Component\HttpFoundation\Response;
+ 
+
+class JWTAuthController extends Controller
+{
+    public $token = true;
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+
+    /**
+     * Register user.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+
+        
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string|min:2|max:100',
+            'last_name' => 'required|string|min:2|max:100',
+            'username' => 'required|string|max:100|unique:users',
+            'password' => 'required|string|min:6',
+            'c_password' => 'required|same:password', 
+            'dob' => 'required|string|',
+            'country' => 'required|string|',
+            'city' => 'required|string|',
+            'phone' => 'required|string|',
+            'gender' => 'required|string|',
+            // 'is_admin' => 'required|string|confirmed|min:6',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json(["message" => "Validator Failed ! Check your submiited values again!"]);
+        }
+
+     
+        $user = new User;
+
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->username = $request->username;
+        $user->password = bcrypt( $request->password);
+        $user->dob = $request->dob;
+        $user->country = $request->country;
+        $user->city = $request->city;
+        $user->phone = $request->phone;
+        $user->gender = $request->gender;
+        $user->is_admin = 0;
+
+        $user->save();
+
+        $input = $request->only('username', 'password');  
+        $jwt_token = JWTAuth::attempt($input);
+
+
+        return response()->json([
+            'message' => 'User successfully registered',
+            'token' => $jwt_token,
+        ], Response::HTTP_OK);
+    }
+
+    public function login(Request $request)
+    {
+        $input = $request->only('username', 'password');
+        $jwt_token = null;
+  
+        if (!$jwt_token = JWTAuth::attempt($input)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Usernname or Password',
+            ]);
+        }
+        $user = Auth::user();
+        return response()->json([
+            'success' => true,
+            'token' => $jwt_token,
+            'is_admin' => $user->is_admin,
+        ]);
+    }
+
+    public function logout() {
+        Auth::guard('api')->logout();
+    
+        return response()->json([
+            'status' => 'success',
+            'message' => 'logout'
+        ], 200);
+    }    
+    
+    public function getLoggedInUser(){
+        return response()->json(auth()->user());
+    }
+    
+    public function refresh(){
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    protected function respondWithToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
+    
+  
+  
+}
