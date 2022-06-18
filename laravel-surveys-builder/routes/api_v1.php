9e4a8cf245797af3d2621fd800e0ac6ca2aa0906
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-06-18 14:43:47 +0300

    feat : login and logout routes

--- laravel-surveys-builder/routes/api_v1.php
+++ laravel-surveys-builder/routes/api_v1.php
@@ -7,7 +7,13 @@ use App\Http\Controllers\Api\V1\Auth\JWTAuthController;
 
 
 
-    
-   
- 
+    Route::post('/login',[JWTAuthController::class, 'login'])->name("login");
+        
+    Route::group(['middleware' => 'auth.jwt'], function($router) {
+        
+        Route::post('/logout',[JWTAuthController::class, 'logout'])->name("logout");
+        
+        
+    });
+
 
