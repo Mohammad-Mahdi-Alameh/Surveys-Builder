db0874a3287228188812662abf3ce3b1aeb19f41
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-06-19 10:20:36 +0300

    feat : link the front end with the get _question_of_specific_survey api

--- react-surveys-builder/src/App.js
+++ react-surveys-builder/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import React, { useState, useEffect } from "react";
 function App() {
   const [surveys, setSurveys] = useState([]);
+  const [questions, setQuestions] = useState([]);
+
+  //Getting all surveys from db
   const fetchSurveys = async () => {
     try {
       const res = await fetch("http://127.0.0.1:8000/api/v1/user/get_surveys");
@@ -20,6 +23,26 @@ function App() {
       setSurveys(serverSurveys);
     };
     getSurveys();
+  }, []);
+  
+  //getting all questions of certain survey from db
+  const fetchQuestions = async () => {
+    try {
+      const res = await fetch("http://127.0.0.1:8000/api/v1/user/get_questions/test");
+      const data = await res.json();
+      console.log(data);
+      // return data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const getQuestions = async () => {
+      const serverQuestions = await fetchQuestions();
+      setQuestions(serverQuestions);
+    };
+    getQuestions();
   }, []);
   return (
 
