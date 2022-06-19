6a1572deed558ce18ed100503854b6e11302e49d
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-06-19 09:23:17 +0300

    feat : link the front end with the get _surveys api

--- react-surveys-builder/src/App.js
+++ react-surveys-builder/src/App.js
@@ -1,9 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from "react";
 function App() {
+  const [surveys, setSurveys] = useState([]);
+  const fetchSurveys = async () => {
+    try {
+      const res = await fetch("http://127.0.0.1:8000/api/v1/user/get_surveys");
+      const data = await res.json();
+      console.log(data);
+      // return data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const getSurveys = async () => {
+      const serverSurveys = await fetchSurveys();
+      setSurveys(serverSurveys);
+    };
+    getSurveys();
+  }, []);
   return (
+
     <>
+
     </>
   );
 }
